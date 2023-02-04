8b77b907c14845d2a96cf30d31083a6ed2697c5a
LukaleTyson <lukaletyson99@gmail.com>
2023-02-04 14:26:25 +0300

    debugged detail page

--- pages/coin/[id].js
+++ pages/coin/[id].js
@@ -11,10 +11,10 @@ const Coin = ({ coin }) => {
             alt={coin.name}
             className={styles.coin_image}
           />
-          <h1 className={styles.coin_name}>{coin.name}</h1>
-          <p className={styles.coin_ticker}>{coin.symbol}</p>
+          <h1 className={styles.coin_name}>{coin?.name}</h1>
+          <p className={styles.coin_ticker}>{coin?.symbol}</p>
           <p className={styles.coin_current}>
-            {coin.market_data.current_price.usd}
+            {coin?.market_data?.current_price?.usd}
           </p>
         </div>
       </div>
